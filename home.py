4b28269e231b75631d32e117125c325e2755b96a
Francesco Pinizzotto <fpinizzotto@gmail.com>
2024-12-12 20:31:40 +0100

    Eliminata gestione caricamento dipendenze

--- home.py
+++ home.py
@@ -7,26 +7,26 @@ import sys
 import time
 
 st.set_page_config(page_title="Home - Analisi Segnalazioni", page_icon="📄", layout="wide")
-try:
-    import streamlit_random_generator
+# try:
+#     import streamlit_random_generator
 
-# This block executes only on the first run when your package isn't installed
-except ModuleNotFoundError as e:
-    sleep_time = 30
-    dependency_warning = st.warning(
-        f"Installing dependencies, this takes {sleep_time} seconds."
-    )
-    subprocess.Popen(
-        [
-            f"{sys.executable} -m pip install git+https://${{github_token}}@github.com/sTomerG/streamlit_random_generator.git",
-        ],
-        shell=True,
-    )
+# # This block executes only on the first run when your package isn't installed
+# except ModuleNotFoundError as e:
+#     sleep_time = 30
+#     dependency_warning = st.warning(
+#         f"Installing dependencies, this takes {sleep_time} seconds."
+#     )
+#     subprocess.Popen(
+#         [
+#             f"{sys.executable} -m pip install git+https://${{github_token}}@github.com/sTomerG/streamlit_random_generator.git",
+#         ],
+#         shell=True,
+#     )
 
-    # wait for subprocess to install package before running your actual code below
-    time.sleep(sleep_time)
-    # remove the installing dependency warning
-    dependency_warning.empty()
+#     # wait for subprocess to install package before running your actual code below
+#     time.sleep(sleep_time)
+#     # remove the installing dependency warning
+#     dependency_warning.empty()
 
 
 
